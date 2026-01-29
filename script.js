fcee0fb5f6a51fcf35fb266c1dd1080d3faea4c6
jihanaprll <jihan.aprilliats@gmail.com>
2026-01-29 22:58:46 +0700

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-function openModal() {
-  document.getElementById("modal").style.display = "flex";
-}
+gsap.registerPlugin(ScrollTrigger);
 
-function closeModal() {
-  document.getElementById("modal").style.display = "none";
-}
+gsap.utils.toArray(".reveal").forEach(el => {
+  gsap.to(el, {
+    opacity: 1,
+    y: 0,
+    duration: 1.2,
+    ease: "power3.out",
+    scrollTrigger: {
+      trigger: el,
+      start: "top 80%"
+    }
+  });
+});
+
+/* MODAL */
+const modal = document.querySelector(".modal");
+document.querySelector(".open-modal").onclick = () => {
+  modal.classList.add("show");
+};
+document.querySelector(".close").onclick = () => {
+  modal.classList.remove("show");
+};
