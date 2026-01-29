a60cbdcfa93e6066ccf35244f2b09e171505842a
jihanaprll <jihan.aprilliats@gmail.com>
2026-01-29 22:40:04 +0700

    script.js

--- script.js
+++ script.js
@@ -2,3 +2,19 @@
 // (sudah smooth scroll via CSS)
 
 console.log("Banova website loaded");
+
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+  const windowHeight = window.innerHeight;
+  reveals.forEach(section => {
+    const top = section.getBoundingClientRect().top;
+    if (top < windowHeight - 100) {
+      section.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+revealOnScroll();
+
