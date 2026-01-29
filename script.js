7429f3ad848890c5adbfffd90bf1aaa2dad56ebc
jihanaprll <jihan.aprilliats@gmail.com>
2026-01-29 22:52:46 +0700

    script.js

--- script.js
+++ script.js
@@ -1,20 +1,7 @@
-// Script sederhana untuk future enhancement
-// (sudah smooth scroll via CSS)
-
-console.log("Banova website loaded");
-
-const reveals = document.querySelectorAll(".reveal");
-
-function revealOnScroll() {
-  const windowHeight = window.innerHeight;
-  reveals.forEach(section => {
-    const top = section.getBoundingClientRect().top;
-    if (top < windowHeight - 100) {
-      section.classList.add("active");
-    }
-  });
+function openModal() {
+  document.getElementById("modal").style.display = "flex";
 }
 
-window.addEventListener("scroll", revealOnScroll);
-revealOnScroll();
-
+function closeModal() {
+  document.getElementById("modal").style.display = "none";
+}
